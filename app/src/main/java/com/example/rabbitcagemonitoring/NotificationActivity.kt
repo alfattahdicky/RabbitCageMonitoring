5f39ce7070331e46124a7ce2f436a014a69da3fc
alfattahdicky <alfattahdicky2@gmail.com>
2022-06-06 23:47:05 +0700

    Finish Notification History with Get Data from Worker manager & using SharedPreferences Ft. GSON

--- app/src/main/java/com/example/rabbitcagemonitoring/NotificationActivity.kt
+++ app/src/main/java/com/example/rabbitcagemonitoring/NotificationActivity.kt
@@ -1,121 +1,65 @@
 package com.example.rabbitcagemonitoring
 
-import android.app.AlarmManager
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.graphics.BitmapFactory
-import android.os.Build
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
-import android.widget.Button
 import android.widget.ImageButton
-import android.widget.Toast
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
+import android.widget.ListView
+import androidx.fragment.app.Fragment
+import androidx.work.OneTimeWorkRequest
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkInfo
+import androidx.work.WorkManager
 import com.google.firebase.database.*
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.lang.reflect.Type
 import java.text.SimpleDateFormat
 import java.time.LocalTime
 import java.util.*
-
-const val channelID = "Rabbit Cage Monitoring"
-const val titleExtra = "title"
-const val descriptionExtra = "description"
+import kotlin.collections.ArrayList
 
 class NotificationActivity : AppCompatActivity() {
-    private val CHANNEL_ID = "channel_id"
-    private lateinit var database: DatabaseReference
     lateinit var dataTimePref: DataTimePref
     private var TAG: String = "Notification Activity"
+    private var dataNotification: ArrayList<DataNotification> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notification)
+        this.dataTimePref = DataTimePref(applicationContext)
 
+
+        loadData()
+
+
+        val listView: ListView = findViewById(R.id.listViewNotification)
+        listView.adapter = AdapterListView(this, this.dataNotification)
+
+        backToHome()
+    }
+
+    private fun loadData() {
+        val sharedPreferences: SharedPreferences = applicationContext.getSharedPreferences("Notification Prefrences", MODE_PRIVATE)
+
+        val gson = Gson()
+        val json: String? = sharedPreferences.getString("Notification List", null)
+        val type: Type = object : TypeToken<ArrayList<DataNotification?>?>() {}.type
+
+        this.dataNotification =  gson.fromJson(json, type)
+
+        this.dataNotification.reverse()
+    }
+
+    private fun backToHome() {
         val backButtonNotification: ImageButton = findViewById(R.id.backButtonNotification)
-        val btnNotificationTv: Button = findViewById(R.id.btn_notificationTv)
-        val btnNotificationTvTwo: Button = findViewById(R.id.btn_notificationTvTwo)
-
-        dataTimePref = DataTimePref(applicationContext)
-        val dataTime = dataTimePref.getPreferences()
-
-        val time = LocalTime.parse(dataTime.eatDrinkTimeOne)
-
-        Log.d(TAG, time.hour.toString())
-
-        Toast.makeText(this, "$dataTime", Toast.LENGTH_SHORT).show()
-    }
-
-    private fun showNotificationEatDrink() {
-        //  current time
-        val currentTime: String = SimpleDateFormat("HH:mm", Locale.getDefault()).format(Date())
-        val calender = Calendar.getInstance().apply {
-            timeInMillis = System.currentTimeMillis()
-            set(Calendar.HOUR_OF_DAY, 23)
-            set(Calendar.MINUTE, 16)
-        }
-
-        val title = "Waktu Pembersihan"
-        val description = "Waktu pembersihan dilakukan ${calender.timeInMillis}"
-
-        val intent = Intent(this, NotificationReceiver::class.java)
-        intent.putExtra(titleExtra, title)
-        intent.putExtra(descriptionExtra, description)
-
-        val pendingIntent = PendingIntent.getBroadcast(
-            this,
-            System.currentTimeMillis().toInt(),
-            intent,
-            0
-        )
-
-        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-
-        alarmManager.setExactAndAllowWhileIdle(
-            AlarmManager.RTC_WAKEUP,
-            calender.timeInMillis,
-            pendingIntent
-        )
-    }
-
-    private fun createNotificationChannel() {
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = "Notification Title"
-            val descriptionText = "Notification Description"
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
-                description = descriptionText
-            }
-
-            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-        }
-    }
-
-    private fun sendNotification(title: String, description: String) {
-        val intent = Intent(this, MainActivity::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        }
-        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
-
-        val bitmap = BitmapFactory.decodeResource(applicationContext.resources, R.drawable.clock)
-
-        val builder = NotificationCompat.Builder(this, channelID)
-            .setSmallIcon(R.drawable.logo_rabbit)
-            .setContentTitle(title)
-            .setContentText(description)
-            .setLargeIcon(bitmap)
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .setContentIntent(pendingIntent)
-            .setAutoCancel(true)
-
-        val timeId: Long = System.currentTimeMillis()
-
-        with(NotificationManagerCompat.from(this)) {
-            notify(timeId.toInt(), builder.build())
+        backButtonNotification.setOnClickListener {
+            val intent = Intent(this@NotificationActivity, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 
--- app/src/main/java/com/example/rabbitcagemonitoring/WorkerTime.kt
+++ app/src/main/java/com/example/rabbitcagemonitoring/WorkerTime.kt
@@ -1,15 +1,63 @@
 package com.example.rabbitcagemonitoring
 
 import android.content.Context
+import android.content.SharedPreferences
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.work.Data
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import androidx.work.workDataOf
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.lang.reflect.Type
 
 class WorkerTime(val context: Context, val params:WorkerParameters): Worker(context, params) {
+    private var TAG = "Worker Time"
+    private var data: ArrayList<DataNotification> = ArrayList()
+
     override fun doWork(): Result {
         NotificationHelper(context).createNotification(
             inputData.getString("title").toString(),
             inputData.getString("description").toString()
         )
+
+        Log.d(TAG, inputData.getString("title").toString())
+        Log.d(TAG, inputData.getString("description").toString())
+
+        loadData()
+
+        saveDataNotification()
+
         return Result.success()
+    }
+
+    fun loadData(): ArrayList<DataNotification> {
+        val sharedPreferences: SharedPreferences = context.getSharedPreferences("Notification Prefrences", Context.MODE_PRIVATE)
+
+        val gson = Gson()
+        val json: String? = sharedPreferences.getString("Notification List", null)
+        val type: Type = object : TypeToken<java.util.ArrayList<DataNotification?>?>() {}.type
+
+        this.data = gson.fromJson(json, type)
+
+        if(this.data == null) this.data = ArrayList()
+
+        return this.data
+
+    }
+
+    private fun saveDataNotification() {
+        val sharedPreferences: SharedPreferences = context.getSharedPreferences("Notification Prefrences", Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+        val gson = Gson()
+
+        this.data.add(DataNotification(inputData.getString("title").toString(), inputData.getString("description").toString()))
+        val json: String = gson.toJson(this.data)
+
+        Log.d(TAG, json)
+
+        editor.putString("Notification List", json)
+        editor.apply()
     }
 }
