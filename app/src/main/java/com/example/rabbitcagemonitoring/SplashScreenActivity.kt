b67daa900bf3cc49ba911c86b5967dc9f4f62a2e
alfattahdicky <alfattahdicky2@gmail.com>
2022-06-22 08:35:52 +0700

    Add Progress Indicator in Splash Screen

--- app/src/main/java/com/example/rabbitcagemonitoring/SplashScreenActivity.kt
+++ app/src/main/java/com/example/rabbitcagemonitoring/SplashScreenActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import com.google.android.material.progressindicator.CircularProgressIndicator
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity(), View.OnClickListener {
@@ -19,6 +20,11 @@ class SplashScreenActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
+        val loadingIndicator: CircularProgressIndicator  = findViewById(R.id.loading)
+        val btnSplashScreenActivity: Button = findViewById(R.id.button_splash)
+        loadingIndicator.visibility = View.VISIBLE
+        btnSplashScreenActivity.visibility = View.GONE
+
         val mainIntent = Intent(this, MainActivity::class.java)
         startActivity(mainIntent)
         finish()
